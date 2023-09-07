653f4dfa17f02aeda4a4f1dd6b89ddb100790af1
Jesica B <111692238+jesshush@users.noreply.github.com>
2023-09-07 20:46:34 +0530

    Add files via upload

---
+++ Changes(append, extend).py
@@ -0,0 +1,29 @@
+#append - adding in the end
+fruits = ['Mango','Apple','Banana','Orange']
+fruits.append('Pear')
+print(fruits)
+
+
+#concatenate
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+concatenate = list1 + list2
+print(concatenate)
+
+
+#insert - adding at the given index
+fruits = ['Mango','Apple','Banana','Orange']
+fruits.insert(3,'Pear')
+print(fruits)
+
+
+#extend
+list1 = [1, 2, 3]
+list2 = [4, 5, 6, 7]
+list1.extend(list2)
+print(list1)
+
+
+#slicing
+fruits = ['Mango','Apple','Banana','Orange']
+print(fruits[1:3])
